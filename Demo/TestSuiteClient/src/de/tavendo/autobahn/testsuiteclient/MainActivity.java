8fb4cbd74f2e06a8fb1a1a5b0492f1e0aa1c26a0
Tobias Oberstein <tobias.oberstein@tavendo.de>
2012-07-31 19:12:58 +0200

    save config; allow arb. ws uri

--- Demo/TestSuiteClient/src/de/tavendo/autobahn/testsuiteclient/MainActivity.java
+++ Demo/TestSuiteClient/src/de/tavendo/autobahn/testsuiteclient/MainActivity.java
@@ -19,12 +19,14 @@
 package de.tavendo.autobahn.testsuiteclient;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+//import dalvik.system.VMRuntime;
 import de.tavendo.autobahn.WebSocket;
 import de.tavendo.autobahn.WebSocketConnection;
 import de.tavendo.autobahn.WebSocketException;
@@ -34,13 +36,28 @@ import de.tavendo.autobahn.WebSocketOptions;
 public class MainActivity extends Activity {
 
    static final String TAG = "de.tavendo.autobahn.testsuiteclient";
+   
+   private static final String PREFS_NAME = "AutobahnAndroidTestsuiteClient";
+   
+   private SharedPreferences mSettings;
 
-   static EditText mHostname;
-   static EditText mPort;
+   static EditText mWsUri;
    static EditText mAgent;
    static TextView mStatusline;
    static Button mStart;
 
+   private void loadPrefs() {
+      mWsUri.setText(mSettings.getString("wsuri", "ws://192.168.1.128:9001"));
+      mAgent.setText(mSettings.getString("agent", "AutobahnAndroid"));
+   }
+
+   private void savePrefs() {
+      SharedPreferences.Editor editor = mSettings.edit();
+      editor.putString("wsuri", mWsUri.getText().toString());
+      editor.putString("agent", mAgent.getText().toString());
+      editor.commit();
+   }
+	   
    int currCase = 0;
    int lastCase = 0;
 
@@ -51,8 +68,13 @@ public class MainActivity extends Activity {
       try {
          if (currCase == 0) {
 
-            sess.connect("ws://" + mHostname.getText() + ":" + mPort.getText() + "/getCaseCount",
+            sess.connect(mWsUri.getText() + "/getCaseCount",
                   new WebSocketConnectionHandler() {
+               
+                     @Override
+                     public void onOpen() {                        
+                        savePrefs();                        
+                     }
 
                      @Override
                      public void onTextMessage(String payload) {
@@ -74,11 +96,11 @@ public class MainActivity extends Activity {
                  options.setReceiveTextMessagesRaw(true);
                  //options.setValidateIncomingUtf8(false);
                  //options.setMaskClientFrames(false);
-                 //options.setMaxMessagePayloadSize(1*1024*1024);
-                 //options.setMaxFramePayloadSize(1*1024*1024);
+                 options.setMaxMessagePayloadSize(4*1024*1024);
+                 options.setMaxFramePayloadSize(4*1024*1024);
                  //options.setTcpNoDelay(false);
 
-                 sess.connect("ws://" + mHostname.getText() + ":" + mPort.getText() + "/runCase?case=" + currCase + "&agent=" + mAgent.getText(),
+                 sess.connect(mWsUri.getText() + "/runCase?case=" + currCase + "&agent=" + mAgent.getText(),
                        new WebSocketConnectionHandler() {
 
                           @Override
@@ -104,7 +126,7 @@ public class MainActivity extends Activity {
                           }
                  }, options);
             } else {
-                  sess.connect("ws://" + mHostname.getText() + ":" + mPort.getText() + "/updateReports?agent=" + mAgent.getText(),
+                  sess.connect(mWsUri.getText() + "/updateReports?agent=" + mAgent.getText(),
                         new WebSocketConnectionHandler() {
 
                            @Override
@@ -128,15 +150,20 @@ public class MainActivity extends Activity {
 
    @Override
    public void onCreate(Bundle savedInstanceState) {
+	   
+	  //VMRuntime heap = VMRuntime.getRuntime();
+	  //heap.setMinimumHeapSize(128 * 1024 * 1024);
 
       super.onCreate(savedInstanceState);
 
       setContentView(R.layout.main);
 
-      mHostname = (EditText) findViewById(R.id.hostname);
-      mPort = (EditText) findViewById(R.id.port);
+      mWsUri = (EditText) findViewById(R.id.wsuri);
       mAgent = (EditText) findViewById(R.id.agent);
       mStatusline = (TextView) findViewById(R.id.statusline);
+
+      mSettings = getSharedPreferences(PREFS_NAME, 0);
+      loadPrefs();
 
       mStart = (Button) findViewById(R.id.start);
       mStart.setOnClickListener(new Button.OnClickListener() {
