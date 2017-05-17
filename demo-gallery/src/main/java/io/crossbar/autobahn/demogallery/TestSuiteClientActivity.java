c4872b9607d479c9ad72937c211d5322f81b0f5f
Omer Akram <om26er@gmail.com>
2017-05-17 17:55:52 +0500

    Ensure the testclient is able to run all tests

--- demo-gallery/src/main/java/io/crossbar/autobahn/demogallery/TestSuiteClientActivity.java
+++ demo-gallery/src/main/java/io/crossbar/autobahn/demogallery/TestSuiteClientActivity.java
@@ -118,6 +118,7 @@ public class TestSuiteClientActivity extends AppCompatActivity implements View.O
     }
 
     private void runTest() throws WebSocketException {
+        final boolean[] receivedClose = new boolean[1];
         final WebSocketConnection webSocket = new WebSocketConnection();
         webSocket.connect(mWsUri.getText() + "/runCase?case=" + currentCase + "&agent=" + mAgent.getText(),
                 new WebSocketConnectionHandler() {
@@ -139,10 +140,14 @@ public class TestSuiteClientActivity extends AppCompatActivity implements View.O
 
                     @Override
                     public void onClose(int code, String reason) {
-                        System.out.println("RAN TEST: " + reason);
-                        mStatusLine.setText("Test case " + currentCase + "/" + lastCase + " finished.");
-                        currentCase += 1;
-                        processNext();
+                        // XXX om26er. Temporary workaround a bug where onClose is closed multiple times,
+                        // causing the test run counter to go haywire.
+                        if (!receivedClose[0]) {
+                            receivedClose[0] = true;
+                            mStatusLine.setText("Test case " + currentCase + "/" + lastCase + " finished.");
+                            currentCase += 1;
+                            processNext();
+                        }
                     }
                 }, mOptions);
     }
@@ -181,7 +186,6 @@ public class TestSuiteClientActivity extends AppCompatActivity implements View.O
 
                     @Override
                     public void onClose(int code, String reason) {
-                        System.out.println("GOT CASE COUNT");
                         mStatusLine.setText("Ok, will run " + lastCase + " cases.");
                         currentCase += 1;
                         processNext();
