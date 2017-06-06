731b39b37528e866f3e36e2d908dc23bb1c69456
Omer Akram <om26er@gmail.com>
2017-06-06 19:58:46 +0500

    Simplify the interface

--- autobahn/src/main/java/io/crossbar/autobahn/wamp/interfaces/ISession.java
+++ autobahn/src/main/java/io/crossbar/autobahn/wamp/interfaces/ISession.java
@@ -16,25 +16,19 @@ import io.crossbar.autobahn.wamp.types.SubscribeOptions;
 import io.crossbar.autobahn.wamp.types.Subscription;
 
 public interface ISession {
-    int STATE_CONNECTED = 1;
-    int STATE_CONNECTED_RESUMING = 2;
-    int STATE_DISCONNECTED = 3;
-    int STATE_DISCONNECTED_RESUMABLE = 4;
-    int STATE_HELLO_SENT = 5;
-    int STATE_AUTHENTICATE_SENT = 6;
-    int STATE_JOINED = 7;
-    int STATE_READY = 8;
-    int STATE_GOODBYE_SENT = 9;
-    int STATE_SENT = 10;
 
     CompletableFuture<Subscription> subscribe(ISubscribeHandler handler, String topic, SubscribeOptions options);
+
     CompletableFuture<PublicationResult> publish(String topic,
                                                  List<Object> args,
                                                  Map<String, Object> kwargs,
                                                  PublishOptions options);
+
     CompletableFuture<Registration> register(IEndpoint endpoint, String procedure, RegisterOptions options);
+
     CompletableFuture<CallResult> call(String procedure,
                                        List<Object> args,
                                        Map<String, Object> kwargs,
                                        CallOptions options);
+
 }
