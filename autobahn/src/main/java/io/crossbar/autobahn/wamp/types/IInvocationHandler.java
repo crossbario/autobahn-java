d85a50f8a7538d025873d8d568f37b91cdc2b9b5
Omer Akram <om26er@gmail.com>
2017-06-14 03:47:32 +0500

    Partially fix master

--- autobahn/src/main/java/io/crossbar/autobahn/wamp/types/IInvocationHandler.java
+++ autobahn/src/main/java/io/crossbar/autobahn/wamp/types/IInvocationHandler.java
@@ -3,7 +3,13 @@ package io.crossbar.autobahn.wamp.types;
 import java.util.List;
 import java.util.Map;
 
+//@FunctionalInterface
+//public interface IInvocationHandler {
+//    CompletableFuture<InvocationResult> run(List<Object> args, Map<String, Object> kwargs, InvocationDetails details);
+//}
+
 @FunctionalInterface
-public interface IInvocationHandler {
-    CompletableFuture<InvocationResult> run(List<Object> args, Map<String, Object> kwargs, InvocationDetails details);
+public interface IInvocationHandler<R> {
+    // This is a signature for the callback that the user code will provide.
+    R run(List<Object> args, Map<String, Object> kwargs, InvocationDetails details);
 }
