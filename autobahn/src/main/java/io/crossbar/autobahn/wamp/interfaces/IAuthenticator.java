8659b52eead4b9c31ace310e919834306ffe761b
Tobias Oberstein <tobias.oberstein@crossbario.com>
2017-06-18 10:58:21 +0200

    fixes

--- autobahn/src/main/java/io/crossbar/autobahn/wamp/interfaces/IAuthenticator.java
+++ autobahn/src/main/java/io/crossbar/autobahn/wamp/interfaces/IAuthenticator.java
@@ -2,7 +2,7 @@ package io.crossbar.autobahn.wamp.interfaces;
 
 import io.crossbar.autobahn.wamp.Session;
 import io.crossbar.autobahn.wamp.types.Challenge;
-import io.crossbar.autobahn.wamp.types.ChallengeResponse
+import io.crossbar.autobahn.wamp.types.ChallengeResponse;
 
 public interface IAuthenticator {
     CompletableFuture<ChallengeResponse> onChallenge(Session session, Challenge challenge);
--- autobahn/src/main/java/io/crossbar/autobahn/wamp/types/Challenge.java
+++ autobahn/src/main/java/io/crossbar/autobahn/wamp/types/Challenge.java
@@ -16,7 +16,7 @@ public class ChallengeResponse {
     public final String signature;
     public final Map<String, Object> extra;
 
-    public Challenge(String signature, Map<String, Object> extra) {
+    public ChallengeResponse(String signature, Map<String, Object> extra) {
         this.signature = signature;
         this.extra = extra;
     }
