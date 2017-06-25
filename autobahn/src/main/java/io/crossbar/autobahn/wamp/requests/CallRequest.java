e158dd96011d050ee44a3a51af854f919799ef93
Omer Akram <om26er@gmail.com>
2017-06-25 20:30:34 +0500

    Remove unused constructor

--- autobahn/src/main/java/io/crossbar/autobahn/wamp/requests/CallRequest.java
+++ autobahn/src/main/java/io/crossbar/autobahn/wamp/requests/CallRequest.java
@@ -21,14 +21,6 @@ public class CallRequest extends Request {
     public final CompletableFuture onReply;
     public final Class resultType;
 
-    public <T> CallRequest(long request, String procedure, CompletableFuture<T> onReply, CallOptions options) {
-        super(request);
-        this.procedure = procedure;
-        this.options = options;
-        this.onReply = onReply;
-        this.resultType = null;
-    }
-
     public CallRequest(long request, String procedure, CompletableFuture onReply, CallOptions options,
                        Class resultType) {
         super(request);
