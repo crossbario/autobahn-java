7b33c92dc58b99252e99c42231a2db9c33d81ea1
Omer Akram <om26er@gmail.com>
2017-07-04 07:49:10 +0500

    Fix build

--- autobahn/src/main/java/io/crossbar/autobahn/wamp/requests/SubscribeRequest.java
+++ autobahn/src/main/java/io/crossbar/autobahn/wamp/requests/SubscribeRequest.java
@@ -20,10 +20,10 @@ import io.crossbar.autobahn.wamp.types.Subscription;
 public class SubscribeRequest extends Request {
     public final String topic;
     public final CompletableFuture<Subscription> onReply;
-    public final IEventHandler<?> handler;
+    public final IEventHandler handler;
 
     public SubscribeRequest(long request, String topic, CompletableFuture<Subscription> onReply,
-                            IEventHandler<?> handler) {
+                            IEventHandler handler) {
         super(request);
         this.topic = topic;
         this.onReply = onReply;
--- autobahn/src/main/java/io/crossbar/autobahn/wamp/types/Subscription.java
+++ autobahn/src/main/java/io/crossbar/autobahn/wamp/types/Subscription.java
@@ -17,9 +17,9 @@ import io.crossbar.autobahn.wamp.interfaces.IEventHandler;
 public class Subscription {
     public final long subscription;
     public final String topic;
-    public final IEventHandler<?> handler;
+    public final IEventHandler handler;
 
-    public Subscription(long subscription, String topic, IEventHandler<?> handler) {
+    public Subscription(long subscription, String topic, IEventHandler handler) {
         this.subscription = subscription;
         this.topic = topic;
         this.handler = handler;
