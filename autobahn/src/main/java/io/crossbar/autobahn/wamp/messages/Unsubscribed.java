84a7c517ca5b085f0d02c3f5cc5f8ba76eee7a80
Ralph Schaer <ralphschaer@gmail.com>
2017-08-27 03:33:17 +0200

    Comply with the specification

--- autobahn/src/main/java/io/crossbar/autobahn/wamp/messages/Unsubscribed.java
+++ autobahn/src/main/java/io/crossbar/autobahn/wamp/messages/Unsubscribed.java
@@ -12,9 +12,7 @@
 package io.crossbar.autobahn.wamp.messages;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import io.crossbar.autobahn.wamp.interfaces.IMessage;
 import io.crossbar.autobahn.wamp.utils.MessageUtil;
@@ -23,35 +21,15 @@ public class Unsubscribed implements IMessage {
 
     public static final int MESSAGE_TYPE = 35;
 
-    private static final long SUBSCRIPTION_NULL = -1;
     private final long request;
-    private final long subscription;
-    private final String reason;
 
-    public Unsubscribed(long request, long subscription, String reason) {
+    public Unsubscribed(long request) {
         this.request = request;
-        this.subscription = subscription;
-        this.reason = reason;
     }
 
     public static Unsubscribed parse(List<Object> wmsg) {
-        MessageUtil.validateMessage(wmsg, MESSAGE_TYPE, "UNSUBSCRIBED", 2, 3);
-
-        long request = MessageUtil.parseRequestID(wmsg.get(1));
-
-        long subscription = SUBSCRIPTION_NULL;
-        String reason = null;
-        if (wmsg.size() > 2) {
-            Map<String, Object> details = (Map<String, Object>) wmsg.get(2);
-            if (details.containsKey("subscription")) {
-                subscription = (long) details.get("subscription");
-            }
-            if (details.containsKey("reason")) {
-                reason = (String) details.get("reason");
-            }
-        }
-
-        return new Unsubscribed(request, subscription, reason);
+        MessageUtil.validateMessage(wmsg, MESSAGE_TYPE, "UNSUBSCRIBED", 2);
+        return new Unsubscribed(MessageUtil.parseRequestID(wmsg.get(1)));
     }
 
     @Override
@@ -59,16 +37,6 @@ public class Unsubscribed implements IMessage {
         List<Object> marshaled = new ArrayList<>();
         marshaled.add(MESSAGE_TYPE);
         marshaled.add(request);
-        if (subscription != SUBSCRIPTION_NULL || reason != null) {
-            Map<String, Object> details = new HashMap<>();
-            if (reason != null) {
-                details.put("reason", reason);
-            }
-            if (subscription != SUBSCRIPTION_NULL) {
-                details.put("subscription", subscription);
-            }
-            marshaled.add(details);
-        }
         return marshaled;
     }
 }
