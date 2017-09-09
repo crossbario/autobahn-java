848d9c85be5b7c5a936c5ca6ae77d73348244409
Omer Akram <om26er@gmail.com>
2017-09-10 03:02:03 +0500

    More cleaning

--- demo-gallery/src/main/java/io/crossbar/autobahn/demogallery/android/MainActivity.java
+++ demo-gallery/src/main/java/io/crossbar/autobahn/demogallery/android/MainActivity.java
@@ -16,10 +16,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
-import java.util.concurrent.CompletableFuture;
-
 import io.crossbar.autobahn.demogallery.R;
-import io.crossbar.autobahn.wamp.types.ExitInfo;
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
@@ -28,10 +25,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ExampleClient client = new ExampleClient();
-        CompletableFuture<ExitInfo> info = client.main(
-                "ws://192.168.1.6:8080/ws", "realm1");
-        info.thenAccept(System.out::println);
     }
 
     @Override
