e4a161f7287026843c53943b509ff305b172efe2
Omer Akram <om26er@gmail.com>
2017-10-02 20:07:10 +0500

    add script to support older versions of android (#291)

---
+++ enable_old_androids.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+set -e
+
+# Uncomment streamsupport gradle library
+sed -i "s|// implementation 'net.sourceforge.streamsupport:streamsupport-cfuture:|implementation 'net.sourceforge.streamsupport:streamsupport-cfuture:|g" autobahn/build.gradle
+sed -i "s|// implementation 'net.sourceforge.streamsupport:streamsupport-cfuture:|implementation 'net.sourceforge.streamsupport:streamsupport-cfuture:|g" demo-gallery/build.gradle
+
+# Change SDK support to Android 4.0.3
+sed -i 's/minSdkVersion.*/minSdkVersion 15/g' autobahn/build.gradle
+sed -i 's/minSdkVersion.*/minSdkVersion 15/g' demo-gallery/build.gradle
+
+# Replace imports to streamsupport.
+find -name *.java -print0 | xargs -0 sed -i 's/import java.util.concurrent.CompletableFuture;/import java8.util.concurrent.CompletableFuture;/g'
+find -name *.java -print0 | xargs -0 sed -i 's/import java.util.concurrent.CompletionException;/import java8.util.concurrent.CompletionException;/g'
+find -name *.java -print0 | xargs -0 sed -i 's/import java.util.concurrent.ForkJoinPool;/import java8.util.concurrent.ForkJoinPool;/g'
+find -name *.java -print0 | xargs -0 sed -i 's/import java.util.function.BiConsumer;/import java8.util.function.BiConsumer;/g'
+find -name *.java -print0 | xargs -0 sed -i 's/import java.util.function.BiFunction;/import java8.util.function.BiFunction;/g'
+find -name *.java -print0 | xargs -0 sed -i 's/import java.util.function.Consumer;/import java8.util.function.Consumer;/g'
+find -name *.java -print0 | xargs -0 sed -i 's/import java.util.function.Function;/import java8.util.function.Function;/g'
+find -name *.java -print0 | xargs -0 sed -i 's/import java.util.function.Supplier;/import java8.util.function.Supplier;/g'
