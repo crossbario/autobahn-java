034b5bc6ebd8b1f1f3617923a3565733016599c3
Omer Akram <omer.akram@crossbario.com>
2017-10-02 21:20:39 +0500

    Point to right file

--- changelog_gen.sh
+++ changelog_gen.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-echo '# Release' $(git describe --abbrev=0)'\n' >> CHANGELOG
-git log $(git describe --abbrev=0)..HEAD --pretty=format:"  * %s" >> CHANGELOG
-echo '\n\n' >> CHANGELOG
+echo '# Release' $(git describe --abbrev=0)'\n' >> CHANGELOG.md
+git log $(git describe --abbrev=0)..HEAD --pretty=format:"  * %s" >> CHANGELOG.md
+echo '\n\n' >> CHANGELOG.md
 
