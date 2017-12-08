486896f99494bd53d6c095645696ff26d6d31a00
Omer Akram <omer.akram@crossbario.com>
2017-12-08 21:25:48 +0500

    Cleanup Auth code

--- autobahn/src/main/java/io/crossbar/autobahn/wamp/auth/ChallengeResponseAuth.java
+++ autobahn/src/main/java/io/crossbar/autobahn/wamp/auth/ChallengeResponseAuth.java
@@ -1,8 +1,6 @@
 package io.crossbar.autobahn.wamp.auth;
 
 import java.io.UnsupportedEncodingException;
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.util.Map;
@@ -15,7 +13,7 @@ import io.crossbar.autobahn.wamp.Session;
 import io.crossbar.autobahn.wamp.interfaces.IAuthenticator;
 import io.crossbar.autobahn.wamp.types.Challenge;
 import io.crossbar.autobahn.wamp.types.ChallengeResponse;
-import io.crossbar.autobahn.wamp.utils.Platform;
+import io.crossbar.autobahn.wamp.utils.AuthUtil;
 
 public class ChallengeResponseAuth implements IAuthenticator {
     public static final String authmethod = "wampcra";
@@ -24,8 +22,7 @@ public class ChallengeResponseAuth implements IAuthenticator {
     public final Map<String, Object> authextra;
     public final String secret;
 
-    private Mac sha256_HMAC;
-    private Class mBase64Class;
+    private Mac sha256HMAC;
 
     public ChallengeResponseAuth(String authid, String secret) {
         this(authid, secret, null);
@@ -37,50 +34,21 @@ public class ChallengeResponseAuth implements IAuthenticator {
         this.authextra = authextra;
         try {
             SecretKeySpec secretKey = new SecretKeySpec(secret.getBytes("UTF-8"), "HmacSHA256");
-            sha256_HMAC = Mac.getInstance("HmacSHA256");
-            sha256_HMAC.init(secretKey);
-            if (Platform.isAndroid()) {
-                mBase64Class = Class.forName("android.util.Base64");
-            } else {
-                // XXX - this adds a dependency on Java8 for non-android systems.
-                // maybe find a way that works everywhere ?
-                mBase64Class = Class.forName("java.util.Base64");
-            }
-        } catch (NoSuchAlgorithmException | UnsupportedEncodingException | InvalidKeyException
-                | ClassNotFoundException e) {
+            sha256HMAC = Mac.getInstance("HmacSHA256");
+            sha256HMAC.init(secretKey);
+        } catch (NoSuchAlgorithmException | UnsupportedEncodingException | InvalidKeyException e) {
             throw new RuntimeException(e);
         }
     }
 
     @Override
     public CompletableFuture<ChallengeResponse> onChallenge(Session session, Challenge challenge) {
-        String ch = challenge.extra.get("challenge").toString();
-        String hash = null;
+        String ch = (String) challenge.extra.get("challenge");
         try {
-            hash = encodeToString(ch.getBytes("UTF-8"));
+            String hash = AuthUtil.encodeToString(sha256HMAC.doFinal(ch.getBytes("UTF-8")));
+            return CompletableFuture.completedFuture(new ChallengeResponse(hash, authextra));
         } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return CompletableFuture.completedFuture(new ChallengeResponse(hash, authextra));
-    }
-
-    private String encodeToString(byte[] challenge) throws Exception {
-        if (Platform.isAndroid()) {
-            // Base64.encodeToString(sha256_HMAC.doFinal(challenge), Base64.DEFAULT).trim()
-            // Equivalent of the above commented line of code but using reflections
-            // so that this class works on both android and non-android systems.
-            Method method = mBase64Class.getMethod("encodeToString", byte[].class, int.class);
-            Field field = mBase64Class.getField("DEFAULT");
-            String result = (String) method.invoke(null, sha256_HMAC.doFinal(challenge),
-                    field.getInt(mBase64Class));
-            return result.trim();
-        } else {
-            // Base64.getEncoder().encodeToString(sha256_HMAC.doFinal(challenge)).trim();
-            Object encoderObject = mBase64Class.getMethod("getEncoder").invoke(null);
-            String result = (String) encoderObject.getClass().getMethod(
-                    "encodeToString", byte[].class).invoke(
-                    encoderObject, sha256_HMAC.doFinal(challenge));
-            return result.trim();
+            throw new RuntimeException(e);
         }
     }
 
--- autobahn/src/main/java/io/crossbar/autobahn/wamp/auth/CryptosignAuth.java
+++ autobahn/src/main/java/io/crossbar/autobahn/wamp/auth/CryptosignAuth.java
@@ -2,9 +2,6 @@ package io.crossbar.autobahn.wamp.auth;
 
 import org.libsodium.jni.keys.SigningKey;
 
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
-import java.util.Formatter;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 
@@ -12,7 +9,7 @@ import io.crossbar.autobahn.wamp.Session;
 import io.crossbar.autobahn.wamp.interfaces.IAuthenticator;
 import io.crossbar.autobahn.wamp.types.Challenge;
 import io.crossbar.autobahn.wamp.types.ChallengeResponse;
-import io.crossbar.autobahn.wamp.utils.Platform;
+import io.crossbar.autobahn.wamp.utils.AuthUtil;
 
 public class CryptosignAuth implements IAuthenticator {
     public static final String authmethod = "cryptosign";
@@ -21,7 +18,7 @@ public class CryptosignAuth implements IAuthenticator {
     public final Map<String, Object> authextra;
     public final String privkey;
 
-    private Class mBase64Class;
+    private final byte[] privateKeyRaw;
 
     public CryptosignAuth(String authid, String privkey) {
         this(authid, privkey, null);
@@ -32,63 +29,24 @@ public class CryptosignAuth implements IAuthenticator {
         this.privkey = privkey;
         this.authextra = authextra;
         try {
-            if (Platform.isAndroid()) {
-                mBase64Class = Class.forName("android.util.Base64");
-            } else {
-                mBase64Class = Class.forName("java.util.Base64");
-            }
-        } catch (ClassNotFoundException e) {
+            privateKeyRaw = AuthUtil.decodeString(privkey);
+        } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
 
     @Override
     public CompletableFuture<ChallengeResponse> onChallenge(Session session, Challenge challenge) {
-        String privateKey = "GJG8MKm/s4sk6uIM/ACynPO8rLeWEw3JDyvDQVoM7xY=";
-        try {
-            byte[] privateKeyBytes = decodeString(privateKey);
-            byte[] rawChallenge = getBinary((String) challenge.extra.get("challenge"));
+        String hexChallenge = (String) challenge.extra.get("challenge");
+        byte[] rawChallenge = AuthUtil.toBinary(hexChallenge);
 
-            SigningKey key = new SigningKey(privateKeyBytes);
-            byte[] signed = key.sign(rawChallenge);
+        SigningKey key = new SigningKey(privateKeyRaw);
+        byte[] signed = key.sign(rawChallenge);
 
-            String signatureHex = asHex(signed);
-            String dataHex = asHex(rawChallenge);
-            String res = signatureHex+dataHex;
+        String signatureHex = AuthUtil.toHexString(signed);
+        String res = signatureHex + hexChallenge;
 
-            return CompletableFuture.completedFuture(new ChallengeResponse(res, null));
-        } catch (Exception e) {
-            e.printStackTrace();
-            return CompletableFuture.completedFuture(new ChallengeResponse(null, null));
-        }
-    }
-
-    private byte[] getBinary(String s) {
-        int len = s.length();
-        byte[] data = new byte[len / 2];
-        for (int i = 0; i < len; i += 2)
-            data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4) +
-                    Character.digit(s.charAt(i + 1), 16));
-        return data;
-    }
-
-    private String asHex(byte[] buf) {
-        Formatter formatter = new Formatter();
-        for (byte b : buf)
-            formatter.format("%02x", b);
-        return formatter.toString();
-    }
-
-    private byte[] decodeString(String privateKey) throws Exception {
-        if (Platform.isAndroid()) {
-            Method method = mBase64Class.getMethod("decode", String.class, int.class);
-            Field field = mBase64Class.getField("DEFAULT");
-            return (byte[]) method.invoke(null, privateKey, field.getInt(mBase64Class));
-        } else {
-            Object encoderObject = mBase64Class.getMethod("getDecoder").invoke(null);
-            return (byte[]) encoderObject.getClass().getMethod(
-                    "decode", String.class).invoke(encoderObject, privateKey);
-        }
+        return CompletableFuture.completedFuture(new ChallengeResponse(res, null));
     }
 
     @Override
