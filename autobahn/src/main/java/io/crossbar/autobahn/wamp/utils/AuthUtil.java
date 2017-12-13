20d8578dc7cb83bb242f3f669649145fb4e0d2c4
Omer Akram <om26er@gmail.com>
2017-12-14 04:07:11 +0500

    Read openssh private key files (#350)
    * Implement reading openssh private key files
    
    * Read ed_25519 key from file
    
    * update variable name

--- autobahn/src/main/java/io/crossbar/autobahn/wamp/utils/AuthUtil.java
+++ autobahn/src/main/java/io/crossbar/autobahn/wamp/utils/AuthUtil.java
@@ -1,10 +1,22 @@
 package io.crossbar.autobahn.wamp.utils;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Formatter;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class AuthUtil {
+
+    private static final String SSH_BEGIN = "-----BEGIN OPENSSH PRIVATE KEY-----";
+    private static final String SSH_END = "-----END OPENSSH PRIVATE KEY-----";
+    private static final String OPENSSH_KEY_V1 = "openssh-key-v1";
 
     private static Class getBase64ClassAndroid() {
         try {
@@ -71,5 +83,80 @@ public class AuthUtil {
                     "encodeToString", byte[].class).invoke(encoderObject, challenge);
             return result.trim();
         }
+    }
+
+    public static Map<String, byte[]> parseOpenSSHFile(File file) throws Exception {
+        BufferedReader br = new BufferedReader(new FileReader(file));
+        String sCurrentLine;
+        List<String> lines = new ArrayList<>();
+        while ((sCurrentLine = br.readLine()) != null) {
+            lines.add(sCurrentLine);
+        }
+        return parseOpenSSHFile(lines);
+    }
+
+    private static Map<String, byte[]> parseOpenSSHFile(List<String> lines) throws Exception {
+        if (!lines.get(0).equals(SSH_BEGIN) || !lines.get(lines.size() - 1).equals(SSH_END)) {
+            throw new RuntimeException("Invalid OPENSSH file");
+        }
+        lines.remove(0);
+        lines.remove(lines.size() - 1);
+        StringBuilder base64StringBuilder = new StringBuilder();
+        for (String line: lines) {
+            base64StringBuilder.append(line);
+        }
+        String base64String = base64StringBuilder.toString();
+        byte[] rawKey = decodeString(base64String);
+
+        byte[] verify = Arrays.copyOfRange(rawKey, 0, OPENSSH_KEY_V1.length());
+        if (!new String(verify).equals(OPENSSH_KEY_V1)) {
+            throw new RuntimeException("Invalid OPENSSH file");
+        }
+
+        boolean occurred = false;
+        int index = 0;
+        for (int i = 0; i < rawKey.length; i++) {
+            if (rawKey[i] == 's'
+                    && rawKey[i + 1] == 's'
+                    && rawKey[i + 2] == 'h'
+                    && rawKey[i + 3] == '-'
+                    && rawKey[i + 4] == 'e'
+                    && rawKey[i + 5] == 'd'
+                    && rawKey[i + 6] == '2'
+                    && rawKey[i + 7] == '5'
+                    && rawKey[i + 8] == '5'
+                    && rawKey[i + 9] == '1'
+                    && rawKey[i + 10] == '9'
+                    && rawKey[i + 11] == 0x00
+                    && rawKey[i + 12] == 0x00
+                    && rawKey[i + 13] == 0x00
+                    && rawKey[i + 14] == ' ') {
+                index = i + 15;
+                if (occurred) {
+                    break;
+                }
+                occurred = true;
+            }
+        }
+
+        byte[] publicKey = Arrays.copyOfRange(rawKey, index, index + 32);
+
+        index += 32;
+        for (int i = index; i < rawKey.length; i++) {
+            if (rawKey[i] == 0x00
+                    && rawKey[i + 1] == 0x00
+                    && rawKey[i + 2] == 0x00
+                    && rawKey[i + 3] == '@') {
+                index = i + 4;
+                break;
+            }
+        }
+
+        byte[] privateKey = Arrays.copyOfRange(rawKey, index, index + 32);
+
+        Map<String, byte[]> result = new HashMap<>();
+        result.put("pubkey", publicKey);
+        result.put("privkey", privateKey);
+        return result;
     }
 }
