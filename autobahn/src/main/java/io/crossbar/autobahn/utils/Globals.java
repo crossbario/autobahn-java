53df2eda0ef1a71d0279813cc6e6c9cae9e9a447
Omer Akram <om26er@gmail.com>
2018-04-30 23:48:28 +0500

    log when pong is received

--- autobahn/src/main/java/io/crossbar/autobahn/utils/Globals.java
+++ autobahn/src/main/java/io/crossbar/autobahn/utils/Globals.java
@@ -1,5 +1,6 @@
 package io.crossbar.autobahn.utils;
 
 public class Globals {
-    public static final boolean DEBUG = false;
+    // Default set to true, this is set to false by the release script.
+    public static final boolean DEBUG = true;
 }
