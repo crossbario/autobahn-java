6682faa41ea345c4ec41d7eeb19ed6994bb17e09
Omer Akram <om26er@gmail.com>
2020-05-09 04:28:34 +0500

    cleaning

--- demo-gallery/src/main/java/io/crossbar/autobahn/demogallery/android/MainActivity.java
+++ demo-gallery/src/main/java/io/crossbar/autobahn/demogallery/android/MainActivity.java
@@ -18,10 +18,6 @@ import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 
 import io.crossbar.autobahn.demogallery.R;
-import io.crossbar.autobahn.websocket.WebSocketConnection;
-import io.crossbar.autobahn.websocket.WebSocketConnectionHandler;
-import io.crossbar.autobahn.websocket.exceptions.WebSocketException;
-import io.crossbar.autobahn.websocket.types.ConnectionResponse;
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
@@ -30,34 +26,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        WebSocketConnection connection = new WebSocketConnection();
-        try {
-            connection.connect("wss://echo.websocket.org", new WebSocketConnectionHandler() {
-                @Override
-                public void onConnect(ConnectionResponse response) {
-                    System.out.println("Connected to server");
-                }
-
-                @Override
-                public void onOpen() {
-                    connection.sendMessage("Echo with Autobahn");
-                }
-
-                @Override
-                public void onClose(int code, String reason) {
-                    System.out.println("Connection closed");
-                }
-
-                @Override
-                public void onMessage(String payload) {
-                    System.out.println("Received message: " + payload);
-                    connection.sendMessage(payload);
-                }
-            });
-        } catch (WebSocketException e) {
-            e.printStackTrace();
-        }
     }
 
     @Override
