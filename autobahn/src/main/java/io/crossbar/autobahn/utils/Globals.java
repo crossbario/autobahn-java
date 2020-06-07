37d2cf7ffcfd979afd311d6eb29a45ab354ea805
Omer Akram <om26er@gmail.com>
2020-06-08 01:35:32 +0500

    update Makefile

--- autobahn/src/main/java/io/crossbar/autobahn/utils/Globals.java
+++ autobahn/src/main/java/io/crossbar/autobahn/utils/Globals.java
@@ -13,5 +13,5 @@ package io.crossbar.autobahn.utils;
 
 public class Globals {
     // Default set to true, this is set to false by the release script.
-    public static final boolean DEBUG = false;
+    public static final boolean DEBUG = true;
 }
