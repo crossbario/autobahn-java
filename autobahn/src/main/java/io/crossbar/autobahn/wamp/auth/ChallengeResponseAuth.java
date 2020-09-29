c4817a018ef1da08fcf64f918949993e1704c33c
Omer Akram <om26er@gmail.com>
2020-09-30 01:15:59 +0500

    implement WAMPCRA password salting

--- autobahn/src/main/java/io/crossbar/autobahn/wamp/auth/ChallengeResponseAuth.java
+++ autobahn/src/main/java/io/crossbar/autobahn/wamp/auth/ChallengeResponseAuth.java
@@ -3,10 +3,13 @@ package io.crossbar.autobahn.wamp.auth;
 import java.io.UnsupportedEncodingException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 
 import javax.crypto.Mac;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
 import javax.crypto.spec.SecretKeySpec;
 
 import io.crossbar.autobahn.wamp.Session;
@@ -17,6 +20,8 @@ import io.crossbar.autobahn.utils.AuthUtil;
 
 public class ChallengeResponseAuth implements IAuthenticator {
     public static final String authmethod = "wampcra";
+
+    private static final String PBKDF2_ALGORITHM = "PBKDF2WithHmacSHA1";
 
     public final String authid;
     public final String authrole;
@@ -48,10 +53,22 @@ public class ChallengeResponseAuth implements IAuthenticator {
         }
     }
 
+    private static byte[] pbkdf2(char[] password, byte[] salt, int iterations, int bytes)
+            throws NoSuchAlgorithmException, InvalidKeySpecException {
+        PBEKeySpec spec = new PBEKeySpec(password, salt, iterations, bytes * 8);
+        SecretKeyFactory skf = SecretKeyFactory.getInstance(PBKDF2_ALGORITHM);
+        return skf.generateSecret(spec).getEncoded();
+    }
+
     @Override
     public CompletableFuture<ChallengeResponse> onChallenge(Session session, Challenge challenge) {
         String ch = (String) challenge.extra.get("challenge");
         try {
+            if (challenge.extra.containsKey("salt")) {
+                byte[] key = pbkdf2(ch.toCharArray(), (byte[]) challenge.extra.get("salt"),
+                        (int) challenge.extra.get("iterations"), (int) challenge.extra.get("keylen"));
+                ch = AuthUtil.toHexString(key);
+            }
             String hash = AuthUtil.encodeToString(sha256HMAC.doFinal(ch.getBytes("UTF-8")));
             return CompletableFuture.completedFuture(new ChallengeResponse(hash, authextra));
         } catch (Exception e) {
