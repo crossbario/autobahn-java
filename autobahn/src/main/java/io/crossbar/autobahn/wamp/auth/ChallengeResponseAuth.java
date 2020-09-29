5e8baa325b2fb485cf8ebc5d0161ff88ed0dea91
Omer Akram <om26er@gmail.com>
2020-09-30 04:48:55 +0500

    wamp cra password salting support

--- autobahn/src/main/java/io/crossbar/autobahn/wamp/auth/ChallengeResponseAuth.java
+++ autobahn/src/main/java/io/crossbar/autobahn/wamp/auth/ChallengeResponseAuth.java
@@ -1,7 +1,13 @@
 package io.crossbar.autobahn.wamp.auth;
 
+import org.bouncycastle.crypto.digests.SHA256Digest;
+import org.bouncycastle.crypto.generators.PKCS5S2ParametersGenerator;
+import org.bouncycastle.crypto.params.KeyParameter;
+
 import java.io.UnsupportedEncodingException;
+import java.math.BigInteger;
 import java.security.InvalidKeyException;
+import java.security.Key;
 import java.security.NoSuchAlgorithmException;
 import java.security.spec.InvalidKeySpecException;
 import java.util.Map;
@@ -21,14 +27,12 @@ import io.crossbar.autobahn.utils.AuthUtil;
 public class ChallengeResponseAuth implements IAuthenticator {
     public static final String authmethod = "wampcra";
 
-    private static final String PBKDF2_ALGORITHM = "PBKDF2WithHmacSHA1";
+    private static final String PBKDF2_ALGORITHM = "PBKDF2WithHmacSHA256";
 
     public final String authid;
     public final String authrole;
     public final Map<String, Object> authextra;
     public final String secret;
-
-    private Mac sha256HMAC;
 
     public ChallengeResponseAuth(String authid, String secret) {
         this(authid, secret, null, null);
@@ -44,31 +48,33 @@ public class ChallengeResponseAuth implements IAuthenticator {
         this.authrole = authrole;
         this.secret = secret;
         this.authextra = authextra;
-        try {
-            SecretKeySpec secretKey = new SecretKeySpec(secret.getBytes("UTF-8"), "HmacSHA256");
-            sha256HMAC = Mac.getInstance("HmacSHA256");
-            sha256HMAC.init(secretKey);
-        } catch (NoSuchAlgorithmException | UnsupportedEncodingException | InvalidKeyException e) {
-            throw new RuntimeException(e);
-        }
     }
 
-    private static byte[] pbkdf2(char[] password, byte[] salt, int iterations, int bytes)
-            throws NoSuchAlgorithmException, InvalidKeySpecException {
-        PBEKeySpec spec = new PBEKeySpec(password, salt, iterations, bytes * 8);
-        SecretKeyFactory skf = SecretKeyFactory.getInstance(PBKDF2_ALGORITHM);
-        return skf.generateSecret(spec).getEncoded();
+    private byte[] pbkdf2(String password, String salt, int iterations, int keySize) throws UnsupportedEncodingException {
+        PKCS5S2ParametersGenerator gen = new PKCS5S2ParametersGenerator(new SHA256Digest());
+        gen.init(password.getBytes("UTF-8"), salt.getBytes("UTF-8"), iterations);
+        return ((KeyParameter) gen.generateDerivedParameters(keySize * 8)).getKey();
     }
 
     @Override
     public CompletableFuture<ChallengeResponse> onChallenge(Session session, Challenge challenge) {
-        String ch = (String) challenge.extra.get("challenge");
         try {
+            String ch = (String) challenge.extra.get("challenge");
+            Mac sha256HMAC = Mac.getInstance("HmacSHA256");
+            Key secretKey;
+
             if (challenge.extra.containsKey("salt")) {
-                byte[] key = pbkdf2(ch.toCharArray(), (byte[]) challenge.extra.get("salt"),
+
+                // FIXME: this is broken
+                byte[] key = pbkdf2(secret, (String) challenge.extra.get("salt"),
                         (int) challenge.extra.get("iterations"), (int) challenge.extra.get("keylen"));
-                ch = AuthUtil.toHexString(key);
+                secretKey = new SecretKeySpec(key, "HmacSHA256");
+
+            } else {
+                secretKey = new SecretKeySpec(secret.getBytes("UTF-8"), "HmacSHA256");
             }
+
+            sha256HMAC.init(secretKey);
             String hash = AuthUtil.encodeToString(sha256HMAC.doFinal(ch.getBytes("UTF-8")));
             return CompletableFuture.completedFuture(new ChallengeResponse(hash, authextra));
         } catch (Exception e) {
