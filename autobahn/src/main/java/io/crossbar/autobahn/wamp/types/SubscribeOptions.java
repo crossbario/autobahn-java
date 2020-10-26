2eea7181c504d1e20ff46a6d628c84112ed2c226
Lin Wei <linwei@ifanr.com>
2020-10-26 09:32:25 +0800

    support customize subscribe.options

--- autobahn/src/main/java/io/crossbar/autobahn/wamp/types/SubscribeOptions.java
+++ autobahn/src/main/java/io/crossbar/autobahn/wamp/types/SubscribeOptions.java
@@ -11,14 +11,47 @@
 
 package io.crossbar.autobahn.wamp.types;
 
-public class SubscribeOptions {
-    public String match;
-    public boolean details;
-    public boolean getRetained;
+import java.util.HashMap;
+import java.util.Map;
 
-    public SubscribeOptions(String match, boolean details, boolean getRetained) {
-        this.match = match;
-        this.details = details;
-        this.getRetained = getRetained;
+public class SubscribeOptions extends HashMap<String, Object> {
+
+    public static final String KEY_MATCH = "match";
+    public static final String KEY_GET_RETAINED = "get_retained";
+
+    public SubscribeOptions(String match, boolean getRetained) {
+        putMatch(match);
+        putGetRetained(getRetained);
     }
+
+    public SubscribeOptions(Map<String, Object> origin) {
+        super(origin);
+    }
+
+    public void putMatch(String match) {
+        put(KEY_MATCH, match);
+    }
+
+    public void putGetRetained(boolean getRetained) {
+        put(KEY_GET_RETAINED, getRetained);
+    }
+
+    public String getMatch() {
+        Object value = get(KEY_MATCH);
+        return value instanceof String ? (String) value : null;
+    }
+
+    public Boolean getRetained() {
+        Object value = get(KEY_GET_RETAINED);
+        return value instanceof Boolean ? (Boolean) value : null;
+    }
+
+    public void removeMatch() {
+        remove(KEY_MATCH);
+    }
+
+    public void removeGetRetained() {
+        remove(KEY_GET_RETAINED);
+    }
+
 }
